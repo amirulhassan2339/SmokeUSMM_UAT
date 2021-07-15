fc41b6746535ac4280e4db37425186c11af32a19
Amir <Hafiz.amir@sltn.net>
2021-07-15 18:03:36 +0500

    report folder updated

--- ReportsFolder/report.js
+++ ReportsFolder/report.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Include/features/PatientGrid/CarePlan/Blank.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Include/features/PatientGrid/CarePlan/Based On Patient Medical Record/WIPBySaveCloseBtn.feature");
 formatter.feature({
-  "name": "Care Plan - Blank",
+  "name": "Care Plan - Reject Status Using SaveClose Button",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@Blank"
+      "name": "@BOPMR"
     }
   ]
 });
 formatter.scenarioOutline({
-  "name": "Verify reject status function using save button",
+  "name": "Verify WIP status function using save \u0026 close button",
   "description": "",
   "keyword": "Scenario Outline",
   "tags": [
     {
-      "name": "@SmokeUSMM_RejectedSaveBtn_Blank"
+      "name": "@Smoke_USMM"
     }
   ]
 });
@@ -24,7 +24,7 @@ formatter.step({
   "keyword": "When "
 });
 formatter.step({
-  "name": "I am on PWB",
+  "name": "I am on PWB with \u003cPatient\u003e",
   "keyword": "Then "
 });
 formatter.step({
@@ -36,7 +36,7 @@ formatter.step({
   "keyword": "And "
 });
 formatter.step({
-  "name": "I click on blank",
+  "name": "I click on basedonpatientmedicalrecord",
   "keyword": "And "
 });
 formatter.step({
@@ -52,44 +52,12 @@ formatter.step({
   "keyword": "When "
 });
 formatter.step({
-  "name": "I select work in progress as no",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "I click on save button",
+  "name": "I click on save and close button",
   "keyword": "And "
 });
 formatter.step({
   "name": "I should see success message \u003cSucessMessage\u003e of CP",
   "keyword": "Then "
-});
-formatter.step({
-  "name": "I should see patient \u003cPatient\u003e as patient_name",
-  "keyword": "* "
-});
-formatter.step({
-  "name": "I should see patient data of draft status",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "I should see status as \u003cDraft Status\u003e on CP header",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "I click on reject button_Blank",
-  "keyword": "When "
-});
-formatter.step({
-  "name": "I should see rejected alert popup",
-  "keyword": "Then "
-});
-formatter.step({
-  "name": "I click on proceed button",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "I should see rejected success message",
-  "keyword": "And "
 });
 formatter.step({
   "name": "I click on title from care plan grid",
@@ -100,11 +68,11 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.step({
-  "name": "I should see patient data as read only",
+  "name": "I should see patient data of WIP status",
   "keyword": "And "
 });
 formatter.step({
-  "name": "I should see reject status as \u003cRejected Status\u003e on CP header",
+  "name": "I should see progress status as \u003cWIP Status\u003e on CP header",
   "keyword": "And "
 });
 formatter.examples({
@@ -117,18 +85,16 @@ formatter.examples({
         "Patient",
         "Title",
         "New Status",
-        "Draft Status",
-        "Rejected Status",
+        "WIP Status",
         "SucessMessage"
       ]
     },
     {
       "cells": [
-        "Dermo505, Mac505",
-        "RejectedPatient",
+        "BABCOX, MICHAEL",
+        "WIPPatient",
         "New",
-        "Draft",
-        "Rejected",
+        "WIP",
         "successCare Plan Saved SuccessfullyHide"
       ]
     }
@@ -148,4 +114,164 @@ formatter.step({
 });
 formatter.match({
   "location": "SD_PatientGrid.I_navigate_to_patient_grid()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Verify WIP status function using save \u0026 close button",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@BOPMR"
+    },
+    {
+      "name": "@Smoke_USMM"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search BABCOX, MICHAEL using global search",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SD_SearchPatient.search_Patient(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I am on PWB with BABCOX, MICHAEL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SD_BOPMR.I_am_on_PWB(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on care plan tab",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.click_On_Care_Plan_Tab()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on add new care plan button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.click_On_Add_New_Care_Plan_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on basedonpatientmedicalrecord",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.click_On_BasedOnPatientMedicalRecord()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see patient BABCOX, MICHAEL as patient_name",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SD_BOPMR.verify_Patient_Data(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see intial status as New on CP header",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.I_should_see_intial_status_as_on_CP_header(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter title WIPPatient",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SD_BOPMR.user_Enter_Title_TestingPatient(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on save and close button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.click_On_SaveClose_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see success message successCare Plan Saved SuccessfullyHide of CP",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SD_BOPMR.Close_and_Verify_Alert_Message(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on title from care plan grid",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SD_BOPMR.I_click_on_title_from_care_plan_grid()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see patient BABCOX, MICHAEL as patient_name",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SD_BOPMR.verify_Patient_Data(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see patient data of WIP status",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.I_should_see_patient_data_of_WIP_status()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see progress status as WIP on CP header",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SD_BOPMR.I_should_see_WIPprogress_status_as_on_CP_header(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
 });
